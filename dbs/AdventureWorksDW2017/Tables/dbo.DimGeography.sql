e3e10a53e9c90c7f4767003ed9eead185ba3886d
Brit Ferguson <mf89538@harriscomputer.com>
2019-05-23 17:00:18 -0700

    Commit from new repository.

---
+++ dbs/AdventureWorksDW2017/Tables/dbo.DimGeography.sql
@@ -0,0 +1,19 @@
+CREATE TABLE [dbo].[DimGeography]
+(
+[GeographyKey] [int] NOT NULL IDENTITY(1, 1),
+[City] [nvarchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[StateProvinceCode] [nvarchar] (3) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[StateProvinceName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[CountryRegionCode] [nvarchar] (3) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[EnglishCountryRegionName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[SpanishCountryRegionName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[FrenchCountryRegionName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[PostalCode] [nvarchar] (15) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[SalesTerritoryKey] [int] NULL,
+[IpAddressLocator] [nvarchar] (15) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[DimGeography] ADD CONSTRAINT [PK_DimGeography_GeographyKey] PRIMARY KEY CLUSTERED  ([GeographyKey]) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[DimGeography] ADD CONSTRAINT [FK_DimGeography_DimSalesTerritory] FOREIGN KEY ([SalesTerritoryKey]) REFERENCES [dbo].[DimSalesTerritory] ([SalesTerritoryKey])
+GO
---
+++ dbs/AdventureWorksDW2017/Views/dbo.vAssocSeqLineItems.sql
@@ -0,0 +1,10 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE VIEW [dbo].[vAssocSeqLineItems]
+AS
+SELECT     OrderNumber, LineNumber, Model
+FROM         dbo.vDMPrep
+WHERE     (FiscalYear = '2013')
+GO
